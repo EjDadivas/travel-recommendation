4438a525073547a721699c844280741dd9defc2d
ej <99721216+EjDadivas@users.noreply.github.com>
2023-09-24 17:50:43 +0800

    code clean up

--- api/routes.py
+++ api/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, Request, Response
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from typing import Union
-from .utils import validate_travel_params, convert_to_dict, get_data
+from .utils import validate_travel_params, get_recommendations
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -13,17 +13,14 @@ def redirect_to_recommend():
 
 @router.get("/travel-recommendations")
 async def recommend(request: Request, country: Union[str, None] = None, season: Union[str, None] = None):
+    data = None
     if country is None and season is None:
         return templates.TemplateResponse("home.html",  {"request": request})
 
-    # Validation of query params
     errors: list = validate_travel_params(country, season)
-    if errors:
-        raise HTTPException(status_code=400, detail={"errors": errors})
-
-    # openAI integration
-    data = get_data(country, season);
-
-    return templates.TemplateResponse("home.html", {"request": request, "data" : data})
+    if not errors:
+        data = get_recommendations(country, season)
+        
+    return templates.TemplateResponse("home.html", {"request": request, "data" : data, "errors": errors})
     
 
